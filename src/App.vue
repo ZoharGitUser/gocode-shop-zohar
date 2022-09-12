fc4e4cd8a457e50f0a3f5e561a5b02d5cdf22ce7
Zohar Bergshtein <zoharb@gmail.com>
2022-09-12 15:52:35 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,175 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import AppHeader from "./components/AppHeader.vue";
+import Products from "./components/Products.vue";
+export default defineComponent({
+  components: { AppHeader, Products },
+});
+</script>
+
+<template>
+  <AppHeader />
+  <Products></Products>
+</template>
+
+<style>
+@import url(https://fonts.googleapis.com/css?family=Roboto:400,300,500);
+
+html,
+body,
+div,
+span,
+applet,
+object,
+iframe,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6,
+p,
+blockquote,
+pre,
+a,
+abbr,
+acronym,
+address,
+big,
+cite,
+code,
+del,
+dfn,
+em,
+img,
+ins,
+kbd,
+q,
+s,
+samp,
+small,
+strike,
+strong,
+sub,
+sup,
+tt,
+var,
+b,
+u,
+i,
+center,
+dl,
+dt,
+dd,
+ol,
+ul,
+li,
+fieldset,
+form,
+label,
+legend,
+table,
+caption,
+tbody,
+tfoot,
+thead,
+tr,
+th,
+td,
+article,
+aside,
+canvas,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section,
+summary,
+time,
+mark,
+audio,
+video {
+  margin: 0;
+  padding: 0;
+  border: 0;
+  outline: 0;
+  font-size: 100%;
+  font: inherit;
+  vertical-align: baseline;
+}
+/* HTML5 display-role reset for older browsers */
+article,
+aside,
+details,
+figcaption,
+figure,
+footer,
+header,
+hgroup,
+menu,
+nav,
+section {
+  display: block;
+}
+body {
+  line-height: 1;
+}
+ol,
+ul {
+  list-style: none;
+}
+blockquote,
+q {
+  quotes: none;
+}
+blockquote:before,
+blockquote:after,
+q:before,
+q:after {
+  content: ’’;
+  content: none;
+}
+/* remember to define visible focus styles! 
+:focus{
+  outline:?????;
+} */
+
+/* remember to highlight inserts somehow! */
+ins {
+  text-decoration: none;
+}
+del {
+  text-decoration: line-through;
+}
+
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+}
+
+/*------------------------------------*\
+  $MAIN
+\*------------------------------------*/
+
+
+
+/* Product Layout */
+
+.product-filter {
+  display: flex;
+  padding: 30px 0;
+}
+
+label {
+  color: #666;
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 2em;
+  text-transform: uppercase;
+}
+</style>
+
---
+++ src/components/Products.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+import Product from "./Product.vue";
+export default defineComponent({
+  data() {
+    return {};
+  },
+  components: { Product },
+});
+</script>
+
+<template lang="">
+  <section class="products">
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+    <Product />
+  </section>
+</template>
+
+<style scoped>
+.products {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
